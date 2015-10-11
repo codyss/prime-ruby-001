710b56707d3171e7d3bcea8af557ff6effda2165
codyss <cody.schwarz@gmail.com>
2015-10-11 12:15:21 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  a = 2
+  result = false
+  if number == 2 || number == 3
+    result = true
+  else
+    while a < number
+      if number % a == 0
+        break
+      elsif a == number - 1
+        result = true
+        break  
+      else  
+        a += 1
+      end
+    end
+  end
+  result
+end
